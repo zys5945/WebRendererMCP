e673083a269f3878014f7d5df723d34802e88aae
zys5945 <shawnl9025@gmail.com>
2025-05-31 15:50:09 -0700

    finish implementation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
+import { chromium, Browser, Page } from "playwright";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+
+let BROWSER: Browser | null = null;
+const PAGES: { [tabId: string]: WeakRef<Page> } = {};
 
 const server = new McpServer({
   name: "WebRenderer",
   version: "1.0.0",
 });
 
-server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }: any) => ({
-  content: [{ type: "text", text: String(a + b) }],
-}));
+const SCHEMA = {
+  pageId: z
+    .string()
+    .describe(
+      "ID of the page to use for rendering. Using the same ID will reuse the same page"
+    ),
+  html: z.string().describe("HTML to render"),
+};
+
+server.tool(
+  "renderHTML",
+  "Renders the given HTML in a playwright page. Returns a single text response indicating success or errors",
+  SCHEMA,
+  async ({ pageId, html }) => {
+    try {
+      if (!BROWSER) {
+        BROWSER = await chromium.launch({ headless: false });
+      }
+
+      let page = PAGES[pageId]?.deref();
+      if (!page) {
+        page = await BROWSER.newPage();
+        PAGES[pageId] = new WeakRef(page);
+      }
+
+      await page.setContent(html);
+
+      return {
+        content: [{ type: "text", text: "success" }],
+        isError: false,
+      };
+    } catch (e) {
+      return {
+        content: [{ type: "text", text: String(e) }],
+        isError: true,
+      };
+    }
+  }
+);
 
 const transport = new StdioServerTransport();
 await server.connect(transport);

f3249357fc379ae7a551b544d214e3bb9b6d66cb
zys5945 <shawnl9025@gmail.com>
2025-05-31 18:01:55 -0700

    publish to npm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { chromium, Browser, Page } from "playwright";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
